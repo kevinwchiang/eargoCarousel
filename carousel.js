3b1673292c01ff09d6ea9f7f3c51f6d3689e7dbc
Kevin Chiang <kevinwchiang@gmail.com>
2017-09-25 01:33:39 -0700

    Basic specs complete

---
+++ carousel.js
@@ -0,0 +1,17 @@
+$(document).ready(() => {
+  $('#lightSlider').lightSlider({
+    gallery: true,
+    item: 1,
+    loop: true,
+    slideMargin: 0,
+    thumbItem: 6,
+  });
+
+  $('.button').hover(
+    function () {
+      $(this).addClass('hover');
+    }, function () {
+      $(this).removeClass('hover');
+    },
+  );
+});
